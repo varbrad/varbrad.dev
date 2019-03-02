5352390abdeade09cae38b52347b4cf3a04af1e2
Brad Davies <varbrad@users.noreply.github.com>
2019-03-02 16:10:22 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  const [data, setData] = React.useState(0)
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <p>Current vale is: {data}</p>
+      <button onClick={() => setData(state => state + 1)}>
+        Click to +1
+      </button>
+    </div>
+  )
+}
+
+export default Home
