8a092d282c8f0a33260ea47f7b53744e24d055e0
Brad Davies <varbrad@users.noreply.github.com>
2019-03-03 19:01:45 +0000

    Added in Style-Components lib

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+class MyDocument extends Document {
+  static async getInitialProps(context) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = context.renderPage
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(
+        context,
+      )
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
+
+export default MyDocument
