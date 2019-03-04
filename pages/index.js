2e1aba62d93008d37c713f90fcb5a1a839c69c82
Brad Davies <varbrad@users.noreply.github.com>
2019-03-04 20:45:08 +0000

    Linting

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,19 @@
 import React from 'react'
-import Heading from '../components/ui/Heading'
-import Button from '../components/ui/Button'
 import Section from '../components/layout/Section'
 import AboutMe from '../components/sections/AboutMe'
-import {
-  PATTERN_TRIANGLES,
-  PATTERN_CB,
-  PATTERN_STARS,
-  PATTERN_STRIPES,
-} from '../styles'
 import '../styles/style.scss'
 import Projects from '../components/sections/Projects'
 import Skills from '../components/sections/Skills'
 import Contact from '../components/sections/Contact'
 
-const Home = () => {
-  const [data, setData] = React.useState(0)
-  return (
-    <div>
-      {/* Top thing goes here */}
-      <Section />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Contact />
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <Section />
+    <AboutMe />
+    <Projects />
+    <Skills />
+    <Contact />
+  </div>
+)
 
 export default Home
